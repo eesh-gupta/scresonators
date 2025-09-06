c930b7d3906e9d0400d606e68799e8df047565c0
Shannon Harvey <spharvey3@gmail.com>
2025-09-05 20:54:55 -0700

    clean things up for Jiayi

--- dbs/notion_utils.py
+++ dbs/notion_utils.py
@@ -11,3 +11,27 @@ def get_notion_client():
         secrets = yaml.safe_load(file)
     token = secrets["notion_token"]
     return Notion2PandasClient(auth=token)
+
+
+def get_notion_db():
+    """
+    Returns the Notion database ID from configs/secrets.yml.
+    """
+    with open("../configs/secrets.yml", "r") as file:
+        secrets = yaml.safe_load(file)
+    name_db = secrets["notion_db_id"]
+    n2p = get_notion_client()
+    notion_df = n2p.from_notion_DB_to_dataframe(name_db)
+    return notion_df
+
+
+def send_notion_db(notion_df):
+    """
+    Sends the updated Notion dataframe back to the Notion database.
+    """
+    n2p = get_notion_client()
+    with open("../configs/secrets.yml", "r") as file:
+        secrets = yaml.safe_load(file)
+    name_db = secrets["notion_db_id"]
+    result = n2p.update_notion_DB_from_dataframe(name_db, notion_df)
+    return result
